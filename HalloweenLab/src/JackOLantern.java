80e24a7f55d5040e9930f7b2e59d8596eef35938
Kevin <kliu6151@bths.edu>
2018-10-28 23:17:03 -0400

    Kinda hard

--- HalloweenLab/src/JackOLantern.java
+++ HalloweenLab/src/JackOLantern.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+public class JackOLantern extends java.lang.Object{
+    String[][] faceFeatures;
 
-public class JackOLantern {
+    public JackOLantern(java.lang.String[][] faceFeatures)
+    {
+        this.faceFeatures = faceFeatures;
+    }
+    public void edit(java.lang.String replace, int row, int column){
+        faceFeatures[row][column] = replace;
+    }
+    public void fill(java.lang.String str)
+    {
+
+    }
+    public java.lang.String toString()
+    {
+        String face = "";
+        for(int i = 0;i<faceFeatures.length;i++)
+        {
+            for (int x = 0; x < faceFeatures.length; x++)
+            {
+                face += faceFeatures[i][x];
+            }
+        }
+        return face;
+    }
+
 }
